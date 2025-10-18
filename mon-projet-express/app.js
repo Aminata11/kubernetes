1404704bee936f5161c5b9a830a14c6de16ac24b
Aminata11 <123710156+Aminata11@users.noreply.github.com>
2025-10-18 20:26:08 +0000

    Update app.js

--- mon-projet-express/app.js
+++ mon-projet-express/app.js
@@ -17,13 +17,17 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(express.json());
 
+// Configuration CORS COMPLÈTE
 app.use(cors({
-  origin: "http://localhost:30289"
-})); 
+  origin: ['http://localhost:30289', 'http://192.168.65.3:30289', 'http://kubernetes.docker.internal:30289', 'http://localhost:5173', 'http://127.0.0.1:30289'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-delete-code']
+}));
 
 // Routes API
 app.use('/api', smartphoneRoutes);
 
 // Lancer le serveur
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Serveur lancé sur http://0.0.0.0:${PORT}`));
